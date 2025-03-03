a5b0815f0f9d82414a655bc55b7006114b34007d
dev-jwshin <dev@foryourdev.com>
2025-03-03 16:36:30 +0000

    refactor: 인증 모듈에서 불필요한 서비스 제거
    - AuthService 및 관련 종속성 제거
    - 모듈의 불필요한 프로바이더 및 내보내기 항목 삭제

--- src/api/v1/auth/auth.module.ts
+++ src/api/v1/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { ConfigurationModule } from '../../../config/config.module';
 
 @Module({
   imports: [ConfigurationModule],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [],
+  exports: [],
 })
 export class AuthModule {}
